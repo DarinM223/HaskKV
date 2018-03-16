f01d3f405fe1c5c0880e37d08d9ce7179cd71d58
Darin Minamoto <darinm223@gmail.com>
2018-03-15 18:08:45 -0700

    Fixes connection error in server test

--- test/ServerTest.hs
+++ test/ServerTest.hs
@@ -1,15 +1,15 @@
 module ServerTest (tests) where
 
+import Control.Concurrent.Async (wait, withAsync)
 import Control.Concurrent.Chan
 import Control.Concurrent.MVar
 import Control.Monad.IO.Class
 import Data.Binary
-import Network.Socket hiding (send, recv)
+import Network.Simple.TCP hiding (send, recv)
 import Test.Tasty
 import Test.Tasty.HUnit
 
 import HaskKV.Server
-import HaskKV.TCPConn
 
 import qualified Data.ByteString.Lazy as BS
 import qualified Network.Socket.ByteString.Lazy as NBS
@@ -17,31 +17,34 @@ import qualified Network.Socket.ByteString.Lazy as NBS
 tests :: TestTree
 tests = testGroup "Server tests" [unitTests]
 
-testPort :: PortNumber
-testPort = 4242
-
 unitTests :: TestTree
 unitTests = testGroup "Unit tests"
     [ testCase "Receives message" $ do
         lock <- newMVar ()
-        chan <- newChan
-        let makeParams = \sock -> ServerState { _socket = sock, _broadcast = chan, _sendLock = lock }
-            handler = \_ state -> flip execServerT state $ do
-                maybeMsg <- recv
-                liftIO $ maybeMsg @?= Just (2 :: Int)
-        runServer testPort makeParams (\_ -> return ()) handler
-        sendSock <- connectSocket "localhost" testPort
-        let msg = 2 :: Int
-            encodedMsg = encode msg
-            msgLen = fromIntegral $ BS.length encodedMsg :: MsgLen
-            encodedLen = encode msgLen
-        NBS.send sendSock encodedLen
-        NBS.send sendSock encodedMsg
-        return ()
+        withAsync (server lock) $ \asyncServer ->
+            withAsync (client lock) $ \asyncClient -> do
+                wait $ asyncServer
+                wait $ asyncClient
     ]
+  where
+    client :: MVar () -> IO ()
+    client lock = connect "127.0.0.1" "4242" $ \(sock, _) -> do
+        withMVar lock $ \_ -> do
+            NBS.send sock encodedLen
+            NBS.send sock encodedMsg
+            return ()
+      where
+        msg = 2 :: Int
+        encodedMsg = encode msg
+        msgLen = fromIntegral $ BS.length encodedMsg :: MsgLen
+        encodedLen = encode msgLen
 
-connectSocket :: String -> PortNumber -> IO Socket
-connectSocket host port = do
-    sock <- socket AF_INET Stream 0
-    connect sock (SockAddrInet port iNADDR_ANY)
-    return sock
+    server :: MVar () -> IO ()
+    server lock = listen "127.0.0.1" "4242" $ \(lsock, _) -> do
+        accept lsock $ \(csock, _) -> do
+            chan <- newChan
+            let state = ServerState { _socket = csock, _broadcast = chan, _sendLock = lock }
+            flip execServerT state $ do
+                msg <- recv
+                liftIO $ msg @?= Just (2 :: Int)
+        return ()
