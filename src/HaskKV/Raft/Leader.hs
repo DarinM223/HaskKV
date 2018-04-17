f6f9e14430c857738a6ca6843c8942c699a1610e
Darin Minamoto <darinm223@gmail.com>
2018-04-16 23:07:11 -0700

    Implements AppendEntries sending by leader

--- src/HaskKV/Raft/Leader.hs
+++ src/HaskKV/Raft/Leader.hs
@@ -30,8 +30,7 @@ runLeader = do
             serverID' <- use serverID
 
             stateType._Leader.matchIndex %= ( IM.insert serverID'
-                                            . fromMaybe 0
-                                            . fmap entryIndex
+                                            . maybe 0 entryIndex
                                             $ lastEntry
                                             )
             ids <- serverIds
@@ -44,5 +43,59 @@ runLeader = do
 
 handleLeaderResponse = undefined
 
-sendAppendEntries :: e -> Int -> Int -> m ()
-sendAppendEntries = undefined
+sendAppendEntries :: ( MonadState RaftState m
+                     , LogM e m
+                     , ServerM (RaftMessage e) ServerEvent m
+                     , Entry e
+                     )
+                  => Maybe e
+                  -> Int
+                  -> Int
+                  -> m ()
+sendAppendEntries entry commitIndex' id = do
+    currTerm' <- use currTerm
+    nextIndexes <- preuse (stateType._Leader.nextIndex)
+
+    let lastIndex = maybe 0 entryIndex entry
+        lastTerm  = maybe 0 entryTerm entry
+        nextIndex = IM.lookup id =<< nextIndexes
+
+    entries <- case nextIndex of
+            Just ni -> fromMaybe [] <$> getEntries [] lastIndex ni
+            Nothing -> return []
+
+    serverID' <- use serverID
+    if null entries
+        then send id AppendEntries
+            { _term        = currTerm'
+            , _leaderId    = serverID'
+            , _prevLogIdx  = lastIndex
+            , _prevLogTerm = lastTerm
+            , _entries     = []
+            , _commitIdx   = commitIndex'
+            }
+        else do
+            let firstSendingIdx = entryIndex $ head entries
+            prevEntry <- loadEntry $ prevIndex firstSendingIdx
+            let prevLogIdx  = maybe 0 entryIndex prevEntry
+                prevLogTerm = maybe 0 entryTerm prevEntry
+            send id AppendEntries
+                { _term        = currTerm'
+                , _leaderId    = serverID'
+                , _prevLogIdx  = prevLogIdx
+                , _prevLogTerm = prevLogTerm
+                , _entries     = entries
+                , _commitIdx   = commitIndex'
+                }
+  where
+    getEntries :: (Entry e, LogM e m) => [e] -> Int -> Int -> m (Maybe [e])
+    getEntries entries index nextIndex
+        | index <= nextIndex = return $ Just entries
+        | otherwise = do
+            entry <- loadEntry index
+            case entry of
+                Just e  -> getEntries (e:entries) (prevIndex index) nextIndex
+                Nothing -> return Nothing
+
+prevIndex :: Int -> Int
+prevIndex index = if index <= 0 then 0 else index - 1
