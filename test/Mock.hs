a47cd039aa8181fc1fec6d1f2b28fd9aba9258a4
Darin Minamoto <darinm223@gmail.com>
2018-06-21 19:02:01 -0700

    Implements more instances for the mock monad

--- test/Mock.hs
+++ test/Mock.hs
@@ -4,28 +4,51 @@ module Mock where
 
 import Control.Lens
 import Control.Monad.State
+import Data.Maybe
+import Data.Binary
 import GHC.Records
 import HaskKV.Log
 import HaskKV.Log.Entry
 import HaskKV.Log.InMem
 import HaskKV.Raft
+import HaskKV.Snapshot hiding (HasSnapshotManager)
 import HaskKV.Store hiding (HasStore)
 
+import qualified Control.Monad.State as S
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
 import qualified Data.IntMap as IM
+import qualified Data.Map as M
 
 type K = Int
 type V = StoreValue Int
 type E = LogEntry K V
 type M = RaftMessage E
 
+data MockSnapshot = MockSnapshot
+    { _file   :: String
+    , _index  :: Int
+    , _term   :: Int
+    , _offset :: Int
+    }
+makeFieldsNoPrefix ''MockSnapshot
+
+data MockSnapshotManager = MockSnapshotManager
+    { _completed :: Maybe MockSnapshot
+    , _partial   :: [MockSnapshot]
+    , _chunks    :: IM.IntMap String
+    }
+makeFieldsNoPrefix ''MockSnapshotManager
+
 data MockConfig = MockConfig
-    { _state          :: RaftState
-    , _store          :: StoreData K V E
-    , _tempLog        :: [E]
-    , _messages       :: [M]
-    , _electionTimer  :: Bool
-    , _heartbeatTimer :: Bool
-    , _appliedEntries :: [E]
+    { _state           :: RaftState
+    , _store           :: StoreData K V E
+    , _tempLog         :: [E]
+    , _snapshotManager :: MockSnapshotManager
+    , _messages        :: [M]
+    , _electionTimer   :: Bool
+    , _heartbeatTimer  :: Bool
+    , _appliedEntries  :: [E]
     }
 makeFieldsNoPrefix ''MockConfig
 
@@ -37,8 +60,11 @@ makeFieldsNoPrefix ''MockConfig
 type Servers = [MockConfig]
 
 instance TempLogM E (State MockConfig) where
-    addTemporaryEntry = undefined
-    temporaryEntries = undefined
+    addTemporaryEntry e = tempLog %= (++ [e])
+    temporaryEntries = do
+        log <- use tempLog
+        tempLog .= []
+        return log
 
 instance LogM E (State MockConfig) where
     firstIndex = _lowIdx . _log <$> gets _store
@@ -49,8 +75,48 @@ instance LogM E (State MockConfig) where
     deleteRange a b = store %= (\s -> s { _log = deleteRangeLog a b (_log s) })
 
 instance StorageM K V (State MockConfig) where
-    getValue = undefined
-    setValue = undefined
-    replaceValue = undefined
-    deleteValue = undefined
-    cleanupExpired = undefined
+    getValue k = getKey k <$> gets _store
+    setValue k v = store %= setKey k v
+    replaceValue k v = zoom store $ S.state $ replaceKey k v
+    deleteValue k = store %= deleteKey k
+    cleanupExpired t = store %= cleanupStore t
+
+instance LoadSnapshotM (M.Map K V) (State MockConfig) where
+    loadSnapshot i t m = store %= loadSnapshotStore i t m
+
+instance TakeSnapshotM (State MockConfig) where
+    takeSnapshot = do
+        storeData <- gets _store
+        let firstIndex = _lowIdx $ _log storeData
+            lastIndex  = _highIdx $ _log storeData
+            lastTerm   = entryTerm . fromJust
+                       . IM.lookup lastIndex
+                       . getField @"_entries" . _log
+                       $ storeData
+        createSnapshot lastIndex lastTerm
+        let snapData = B.concat . BL.toChunks . encode . _map $ storeData
+        writeSnapshot 0 snapData lastIndex
+        saveSnapshot lastIndex
+        deleteRange firstIndex lastIndex
+
+instance ApplyEntryM K V E (State MockConfig) where
+    applyEntry entry@LogEntry{ _data = entryData } = do
+        appliedEntries %= (++ [entry])
+        case entryData of
+            Change _ k v -> setValue k v
+            Delete _ k   -> deleteValue k
+            _            -> return ()
+
+{-createSnapshot :: Int -> Int -> m ()-}
+{-writeSnapshot  :: Int -> B.ByteString -> Int -> m ()-}
+{-saveSnapshot   :: Int -> m ()-}
+{-readSnapshot   :: Int -> m (Maybe s)-}
+{-readChunk      :: Int -> Int -> m (Maybe SnapshotChunk)-}
+{-snapshotInfo   :: m (Maybe (Int, Int))-}
+instance SnapshotM (M.Map K V) (State MockConfig) where
+    createSnapshot i t = undefined
+    writeSnapshot offset snapData i = undefined
+    saveSnapshot i = undefined
+    readSnapshot i = undefined
+    readChunk i t = undefined
+    snapshotInfo = undefined
