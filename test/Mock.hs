4363f1df0df99a4149b6ba3881974e70c7a0a30a
Darin Minamoto <darinm223@gmail.com>
2018-06-22 21:29:19 -0700

    runServers only runs through one round of servers, removes WriterT

--- test/Mock.hs
+++ test/Mock.hs
@@ -2,29 +2,22 @@ module Mock where
 
 import Control.Lens
 import Control.Monad.State
-import Control.Monad.Writer
 import HaskKV.Raft
 import Mock.Instances
 
 import qualified Data.IntMap as IM
 
--- TODO(DarinM223): this should be an inspectable event
--- like a message sent or a state change.
-data Event = Event
-
 serverKeys :: MockT (IM.IntMap MockConfig) [Int]
 serverKeys = MockT $ state $ \map -> (IM.keys map, map)
 
--- Infinitely runs through each server and builds a lazy list.
-runServers :: WriterT [Event] (MockT (IM.IntMap MockConfig)) ()
-runServers = do
-    ids <- lift serverKeys
-    let infiniteIds = cycle ids
-    forM_ infiniteIds $ \i -> do
-        s <- lift $ MockT $ preuse (ix i) >>= \case
-            Just s -> pure s
-            _      -> error "Cannot get MockConfig"
-        let (_, s') = runMockT run s
-        lift $ MockT (ix i .= s')
+runServer :: Int -> MockT MockConfig a -> MockT (IM.IntMap MockConfig) a
+runServer i m = do
+    s <- MockT $ preuse (ix i) >>= \case
+        Just s -> pure s
+        _      -> error "Cannot get MockConfig"
+    let (a, s') = runMockT m s
+    MockT (ix i .= s')
+    return a
 
-        -- TODO(DarinM223): add events to the WriterT to inspect
+runServers :: MockT (IM.IntMap MockConfig) ()
+runServers = serverKeys >>= mapM_ (`runServer` run)
