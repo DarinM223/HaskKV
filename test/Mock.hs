5dc7ceb856b042e7a937c9dc3f1718b3e663f763
Darin Minamoto <darinm223@gmail.com>
2018-06-23 01:21:34 -0700

    runServers sends messages after each server run

--- test/Mock.hs
+++ test/Mock.hs
@@ -8,16 +8,32 @@ import Mock.Instances
 import qualified Data.IntMap as IM
 
 serverKeys :: MockT (IM.IntMap MockConfig) [Int]
-serverKeys = MockT $ state $ \map -> (IM.keys map, map)
+serverKeys = MockT $ gets IM.keys
 
-runServer :: Int -> MockT MockConfig a -> MockT (IM.IntMap MockConfig) a
-runServer i m = do
-    s <- MockT $ preuse (ix i) >>= \case
-        Just s -> pure s
-        _      -> error "Cannot get MockConfig"
-    let (a, s') = runMockT m s
-    MockT (ix i .= s')
-    return a
+crashServer :: Int -> MockT (IM.IntMap MockConfig) ()
+crashServer sid = MockT $ modify $ IM.delete sid
+
+dropMessage :: Int -> MockT (IM.IntMap MockConfig) ()
+dropMessage i = void $ runServer i $ MockT $ receivingMsgs %= drop 1
+
+runServer :: Int -> MockT MockConfig a -> MockT (IM.IntMap MockConfig) (Maybe a)
+runServer i m =
+    MockT (preuse (ix i)) >>= \case
+        Just s -> do
+            let (a, s') = runMockT m s
+            MockT (ix i .= s')
+            return $ Just a
+        _ -> return Nothing
+
+flushMessages :: Int -> MockT (IM.IntMap MockConfig) ()
+flushMessages i =
+    MockT (preuse (ix i)) >>= \case
+        Just s -> MockT $ do
+            let messages = _sendingMsgs s
+            (ix i . sendingMsgs) .= IM.empty
+            forM_ (IM.assocs messages) $ \(sid, msgs) ->
+                (ix sid . receivingMsgs) %= (++ msgs)
+        _ -> return ()
 
 runServers :: MockT (IM.IntMap MockConfig) ()
-runServers = serverKeys >>= mapM_ (`runServer` run)
+runServers = serverKeys >>= mapM_ (\i -> runServer i run >> flushMessages i)
